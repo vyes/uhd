149c2b60b572738e056695ff28d7e6a32e17d682
Michael Auchter <michael.auchter@ni.com>
2019-09-09 16:46:14 -0500

    mpm: add tlv_eeprom
    Add support for parsing an eeprom that uses tag-length-value to store
    contents.
    
    Co-authored-by: Michael Auchter <michael.auchter@ni.com>
    Co-authored-by: Virendra Kakade <virendra.kakade@ni.com>
    Co-authored-by: Cristina Fuentes <cristina.fuentes@ni.com>

---
+++ mpm/tools/tlv_eeprom/eeprom-pids.h
@@ -0,0 +1,16 @@
+
+#pragma once
+
+#include <stdint.h>
+
+struct pid_info {
+  uint16_t pid;
+  const char* name;
+  const char* description;
+  uint16_t rev_offset;
+};
+
+const struct pid_info* get_info_from_pid(uint16_t pid);
+const char* get_name_from_pid(uint16_t pid);
+const char* get_description_from_pid(uint16_t pid);
+uint16_t get_rev_offset_from_pid(uint16_t pid);
